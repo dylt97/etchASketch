2f40b4018dff8e543a93887692c19d4a9682f4a7
Dylan Thomas <dylanthomas441@gmail.com>
2023-10-27 00:23:01 -0400

    added an erase effect on rightClick added colorpicker

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let width = document.getElementById('gridContainer').offsetWidth;
-let height = document.getElementById('gridContainer').offsetHeight;
-
 let container = document.getElementById('gridContainer');
+let boxes;
+let color = document.querySelector('#colorSelect');
 
 const gridButton = document.querySelector('#gridBtn');
 gridButton.addEventListener('click', function() {
-    let choice = prompt('What size grid would you use', 'Maximum of 100');
-    createGrid(choice);
+    let choice = parseInt(prompt('What size grid would you use', 'Maximum: 99!'));
+    if (choice < 100) {createGrid(choice)} else {alert('Error Maximum exceeded')};
+    boxes = container.querySelectorAll('div');
 });
 
 function createGrid (gridSize) {
@@ -14,9 +14,39 @@ function createGrid (gridSize) {
     for (let i = 1; i <= gridSize*gridSize; i++) {
         let tile = document.createElement('div');
         container.appendChild(tile);
-        tile.style.border = '1px solid red';
     };
 
     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 };
 
+let isDrawing = false;
+window.addEventListener('mousedown', () => {
+    isDrawing = true;
+});
+window.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+container.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+});
+
+function colorDivs() {
+    boxes = container.querySelectorAll('div');
+
+    container.addEventListener('mousedown', (e) => {
+
+        boxes.forEach(box => box.addEventListener('mouseover', function () {
+            if (isDrawing) {
+                switch (e.button) {
+                    case 0: this.style.backgroundColor = color.value;
+                    break;
+
+                    case 2: this.style.backgroundColor = '';
+                };
+            };
+        }));
+
+    });
+};
+
+colorDivs();
